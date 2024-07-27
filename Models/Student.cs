7571e95610043285e02814a821474d62d150707b
Davis-Cedrick Baseka <davis-cedrickbaseka@Davis-Cedricks-MacBook-Air.local>
2024-07-27 00:15:51 -0400

    Completed

---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Registrar;
+
+    public class Student
+    {
+        private static Random random = new Random();
+        public int Id { get; } = random.Next(100000, 999999);
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Type { get; set; } // "Full Time", "Part Time", or "Coop"
+        public List<Course> Courses { get; set; } = new List<Course>();
+    }
+
